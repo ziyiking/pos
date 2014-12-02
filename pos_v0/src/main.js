3b6b79faa71e7430bababf7eabe42fb5b090ee2f
jzy <ZiyiKing@outlook.com>
2014-12-02 17:47:31 +0800

    finish sum price

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -1,6 +1,6 @@
 function printInventory(items) {
   var itemsText = '';
-
+  var sum = 0 ;
   for(var i = 0; i < items.length; i++) {
     var item = items[i];
 
@@ -9,13 +9,14 @@ function printInventory(items) {
                  '，单价：' + item.price.toFixed(2) +
                  '(元)，小计：' + (item.count * item.price).toFixed(2) +
                  '(元)\n';
-  }
+                 sum += (item.count * item.price);
+                 }
 
   var inventoryText =
     '***<没钱赚商店>购物清单***\n' +
     itemsText +
     '----------------------\n' +
-    '总计：23.00(元)\n' +
+    '总计：' + sum.toFixed(2) + '(元)\n' +
     '**********************';
 
   console.log(inventoryText)
