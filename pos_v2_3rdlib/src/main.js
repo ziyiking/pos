8fdb189d9a79779006fcba2364cf983b4ec22e35
jzy <ZiyiKing@outlook.com>
2014-12-12 17:21:45 +0800

    fix my posv2_3

--- pos_v2_3rdlib/src/main.js
+++ pos_v2_3rdlib/src/main.js
@@ -1,6 +1,5 @@
 function printInventory(tags){
   var cartItems = CartItems.getCartItems(tags);
   var inventoryText = new Inventory(cartItems);
-//  var inventoryText = inventoryText.getInventoryText ;
   console.log(inventoryText.getInventoryText());
 }
