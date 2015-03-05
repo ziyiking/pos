d8fd5cc5810a41b3955c612fc84bed770661f391
jzy <ZiyiKing@outlook.com>
2015-03-06 00:23:58 +0800

    creat scanner_tests.js to test scanner.js

---
+++ pos_v3/__test__/scanner_test.js
@@ -0,0 +1,43 @@
+jest.autoMockOff();
+jest.dontMock('lodash');
+
+describe('Scanner', function() {
+  var Scanner ;
+  beforeEach(function () {
+    Scanner = require('../src/model/scanner.js');
+  var scanner = new Scanner();
+  });
+
+  describe('addCartItems',function() {
+    it('should return one cartItem is a brand', function() {
+      var  result = scanner.addCartItems([{'ITEM000000': 20}]);
+      expect(result[0].item.brand).toEqual('可口可乐');
+    });
+
+    // it('should return two cartItems', function() {
+    //   var result = scanner.addCartItems([{ 'ITEM000000' : 20 },
+    //                                      { 'ITEM000010' : 20 }]);
+    //   expect(result.length).tobe(2);
+    // });
+
+    it('should return two cartItems_brand',function() {
+      var result = scanner.addCartItems([{ 'ITEM000000' : 20 },
+                                         { 'ITEM000010' : 20 }]);
+      expect(result[0].item.brand).toEqual('可口可乐');
+      expect(result[1].item.brand).toEqual('可口可乐');
+    });
+
+    it('should return two cartItems_name',function() {
+      var result = scanner.addCartItems([{ 'ITEM000000' : 20 },
+                                         { 'ITEM000010' : 20 }]);
+      expect(result[0].item.name).toEqual('可口可乐350ml');
+      expect(result[0].item.name).toEqual('可口可乐550ml');
+    });
+
+    it('should return two cartItems_price',function() {
+      var result = scanner.addCartItems([{ 'ITEM000000' : 20 },
+                                         { 'ITEM000010' : 20 }]);
+      expect(result[0].item.price).toEqual(3);
+    });
+  });
+});
