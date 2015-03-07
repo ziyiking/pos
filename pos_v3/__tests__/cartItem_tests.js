af2d68ca530d91e218cd81e3566f05645f4f3ea0
jzy <ZiyiKing@outlook.com>
2015-03-07 17:51:35 +0800

    creat cartItem_test.js to test cartItem.js

---
+++ pos_v3/__tests__/cartItem_tests.js
@@ -0,0 +1,17 @@
+jest.dontMock('lodash');
+jest.dontMock('../src/model/item.js');
+jest.dontMock('../src/model/cartItem.js');
+
+
+describe('CartItem',function() {
+  describe('#getCartItemText',function() {
+    it('should return about Commodity information',function() {
+      var Item = require('../src/model/item.js');
+      var item = new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐');
+      var CartItem = require('../src/mode;/carItem.js');
+      var cartItems = new CartItem(item,20);
+      var result = carItems.getCartItemText();
+      expect(result).toEqual('名称：可口可乐350ml，数量：20瓶，单价：3.00(元)，小计：60.00(元)\n');
+     });
+  });
+});
